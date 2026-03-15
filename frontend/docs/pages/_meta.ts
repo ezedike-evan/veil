64fbc3275eb212b2b4c1d1c7295a4b65842a9dc0
Miracle656 <iupacnumen2020@gmail.com>
2026-03-15 10:52:19 +0100

    fix(docs): migrate _meta.json to _meta.ts (Nextra 3 removed JSON support)

---
+++ frontend/docs/pages/_meta.ts
@@ -0,0 +1,8 @@
+export default {
+  index: 'Introduction',
+  'getting-started': 'Getting Started',
+  architecture: 'Architecture',
+  'contract-api': 'Contract API',
+  'sdk-reference': 'SDK Reference',
+  security: 'Security',
+}
